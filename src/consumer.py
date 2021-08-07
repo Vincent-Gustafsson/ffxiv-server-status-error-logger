631de415dd61a4024a699e9aa39fde709f94b547
Vincent <vincenthonor8lite@gmail.com>
2021-08-07 23:41:00 +0200

    Initial commit + basic-ish consumer

---
+++ src/consumer.py
@@ -0,0 +1,23 @@
+import os
+
+import pika
+
+
+def on_message(channel, method_frame, header_frame, body):
+    print(method_frame.delivery_tag)
+    print(body)
+    print()
+    channel.basic_ack(delivery_tag=method_frame.delivery_tag)
+
+
+params = pika.URLParameters(os.getenv("CLOUDAMQP_URL"))
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+channel.basic_consume('test', on_message)
+try:
+    channel.start_consuming()
+except KeyboardInterrupt:
+    channel.stop_consuming()
+connection.close()
